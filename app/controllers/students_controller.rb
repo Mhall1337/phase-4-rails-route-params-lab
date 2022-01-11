b6a58fb89a0f2f911979d12d2c429935499724e0
Mitchell <Hmitchell.hall@gmail.com>
2022-01-11 12:26:01 -0600

    commit tree a36426d8f36ef60419551076f72722f0a662f580

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,21 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    if(params[:name])
+    students = Student.where("first_name LIKE ? OR last_name LIKE ?" , "#{params[:name]}", "#{params[:name]}")
+    else 
+      students = Student.all
+    end
     render json: students
   end
 
+  def id
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  def name
+    student = Student.find_by(first_name: params[:first_name])
+    render json: student
+  end
 end
